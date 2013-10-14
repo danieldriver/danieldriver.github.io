c0c81efbdc8bfbe4dd19f9de0661e77c3ec3645b
Daniel Driver <mail@danieldriver.com>
2013-10-14 11:42:16 -0400

    use jQuery to place footnotes as sidenotes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-
+// position footnotes in margin 
+$(document).ready(function() {
+  var fnbottom = $("article header").height();
+  $(".footnoteRef").each(function() {
+    var fn = $(this).attr("href");
+    var ref = $(this).position().top;
+    var pos = Math.max(ref,fnbottom);
+    $(".footnotes").css("top", "0px");
+    $(fn).css({
+      position: "absolute",
+      top: pos + "px",
+      left: "0px"
+    });
+    $(fn).find("a:last-child").remove();
+    fnbottom = $(fn).position().top + $(fn).height()
+             + parseInt($(fn).css("margin-bottom"),10) 
+             + parseInt($(fn).css("padding-bottom"),10);
+  });
+});
